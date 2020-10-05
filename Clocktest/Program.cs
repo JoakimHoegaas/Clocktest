d1755fecb02037f651ba6a9022d729ab9e0168e3
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2020-10-05 14:40:31 +0200

    Initial commit

---
+++ Clocktest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Clocktest
+{
+    public class Program
+    {
+        public string Square = "█";
+        // somehow implement this square as output for the algorithm below
+        public void Main(int startX, int y, int deltaX, int deltaY)
+        {
+            var deltaYPerPixel = deltaY / deltaX;
+            /* var steepAngle = false;
+             * if (deltaY > deltaX)
+             * { steepAngle = true; }
+             */
+            var correctY = y;
+            for (int x = startX; x < startX + deltaX; x++)
+            {
+                drawPixel(x, y);
+                correctY += deltaYPerPixel;
+                if (correctY - y > 0.5) y++;
+
+            }
+            
+        }
+    }
+}
